569f4ce5d6fc23e858f67a3b77104fc7b2927e68
unknown <shainarosebisnan@gmail.com>
2025-02-24 06:17:28 -0800

    sample message

---
+++ FIRST_JS/js/operating.js
@@ -0,0 +1,24 @@
+function sum(num1,num2) {
+    const answer = num1 + num2;
+    console.log("The sum is ",answer);
+
+}
+
+function sum(num1,num2) {
+    const answer = num1 - num2;
+    console.log("The difference is ",answer);
+
+}
+
+function sum(num1,num2) {
+    const answer = num1 * num2;
+    console.log("The product is ",answer);
+
+}
+
+function sum(num1,num2) {
+    const answer = num1 / num2;
+    console.log("The quotient is ",answer);
+
+}
+
